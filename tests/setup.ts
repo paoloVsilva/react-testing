ab18d7eaaed91e8bf9138fa39f4d1379facfd0f8
paolosilva <silva.paolo.10@gmail.com>
2025-07-29 13:50:39 +0800

    Implement testing on promise

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,13 @@
 import "@testing-library/jest-dom/vitest";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false, // Default value
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
